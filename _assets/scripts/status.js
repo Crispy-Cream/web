e9b257ef56a0038f7843c84efd0ec1454db44607
Crispy-Cream <tythetechguy06@gmail.com>
2022-01-06 18:17:51 -0600

    added offline color, spotify link

--- _assets/scripts/status.js
+++ _assets/scripts/status.js
@@ -1,26 +1,36 @@
 async function getStatus() {
-    let res = await fetch('https://api.lanyard.rest/v1/users/455139054464270345').then(res => res.json());
-    if (res.data.listening_to_spotify === true) nowPlaying(res);
-    switch (res.data.discord_status) {
-        case "dnd": {
-            document.getElementById('pfp').style['box-shadow'] = "0px 0px 9px #f04747";
-            break;
-        };
-        case "online": {
-            document.getElementById('pfp').style['box-shadow'] = "0px 0px 9px #43b581";
-            break;
-        };
-        case "idle": {
-            document.getElementById('pfp').style['box-shadow'] = "0px 0px 9px #faa61a";
-            break;
-        };
-        case "offline": {
-            document.getElementById('pfp').style['box-shadow'] = "0px 0px 9px #b59f8e";
-            break;
-        };
+  let res = await fetch(
+    "https://api.lanyard.rest/v1/users/455139054464270345"
+  ).then((res) => res.json());
+  if (res.data.listening_to_spotify === true) nowPlaying(res);
+  switch (res.data.discord_status) {
+    case "dnd": {
+      document.getElementById("pfp").style["box-shadow"] =
+        "0px 0px 9px #f04747";
+      break;
     }
+    case "online": {
+      document.getElementById("pfp").style["box-shadow"] =
+        "0px 0px 9px #43b581";
+      break;
+    }
+    case "idle": {
+      document.getElementById("pfp").style["box-shadow"] =
+        "0px 0px 9px #faa61a";
+      break;
+    }
+    case "offline": {
+      document.getElementById("pfp").style["box-shadow"] =
+        "0px 0px 9px #707b88"; // #b59f8e
+      break;
+    }
+  }
 }
 
 async function nowPlaying(res) {
-        document.getElementById("spotify").getElementsByTagName('h2')[0].innerHTML = `<i class="fa-duotone fa-headphones"></i>&ensp;${res.data.spotify.song} by ${res.data.spotify.artist}`;
+  document
+    .getElementById("spotify")
+    .getElementsByTagName(
+      "h2"
+    )[0].innerHTML = `<i class="fa-duotone fa-headphones"></i>&ensp;<a href="https://open.spotify.com/track/${res.data.spotify.track_id}">${res.data.spotify.song}</a> by ${res.data.spotify.artist}`;
 }
