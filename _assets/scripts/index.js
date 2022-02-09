826d39593ade20665dc4418cfeab221e82aff48b
Crispy <61794525+Crispy-Cream@users.noreply.github.com>
2022-02-09 05:42:35 -0600

    New Color Theme

--- _assets/scripts/index.js
+++ _assets/scripts/index.js
@@ -1,101 +1,66 @@
 async function getStatus() {
-  let res = await fetch(
-    "https://api.lanyard.rest/v1/users/455139054464270345"
-  ).then((res) => res.json());
+  let res = await fetch("https://api.lanyard.rest/v1/users/455139054464270345").then((res) => res.json());
 
-  document.getElementById("pfp").style[
-    "background-image"
-  ] = `url("https://cdn.discordapp.com/avatars/455139054464270345/${res.data.discord_user.avatar}.png?size=4096")`;
+  document.getElementById("pfp").style["background-image"] = `url("https://cdn.discordapp.com/avatars/455139054464270345/${res.data.discord_user.avatar}.png?size=4096")`;
 
   CurrentStatus(res.data.activities[0]);
 
   switch (res.data.discord_status) {
-    case "dnd": {
-      document.getElementById("pfp").style["box-shadow"] =
-        "0px 0px 9px #f04747";
-      break;
-    }
-    case "online": {
-      document.getElementById("pfp").style["box-shadow"] =
-        "0px 0px 9px #43b581";
-      break;
-    }
-    case "idle": {
-      document.getElementById("pfp").style["box-shadow"] =
-        "0px 0px 9px #faa61a";
-      break;
-    }
-    case "offline": {
-      document.getElementById("pfp").style["box-shadow"] =
-        "0px 0px 9px #707b88"; // #b59f8e
-      break;
-    }
+      case "dnd": {
+          document.documentElement.style.setProperty("--currentStatus", "#f04747");
+          break;
+      }
+      case "online": {
+          document.documentElement.style.setProperty("--currentStatus", "#43b581");
+          break;
+      }
+      case "idle": {
+          document.documentElement.style.setProperty("--currentStatus", "#faa61a");
+          break;
+      }
+      case "offline": {
+          document.documentElement.style.setProperty("--currentStatus", "#707b88");
+          break;
+      }
   }
 }
 
 async function CurrentStatus(data) {
   if (data === undefined) {
-    document
-      .getElementById("centered")
-      .getElementsByTagName(
-        "p"
-      )[0].innerHTML = `<i class="fa-duotone fa-rocket-launch"></i>&ensp;tysm.dev/github`;
-    return;
+      document.getElementById("centered").getElementsByTagName("p")[0].innerHTML = `<i class="fa-duotone fa-rocket-launch"></i>&ensp;tysm.dev/github`;
+      return;
   } else if (data.id !== "custom") {
-    document
-      .getElementById("centered")
-      .getElementsByTagName(
-        "p"
-      )[0].innerHTML = `<i class="fa-duotone fa-rocket-launch"></i>&ensp;tysm.dev/github`;
-    return;
+      document.getElementById("centered").getElementsByTagName("p")[0].innerHTML = `<i class="fa-duotone fa-rocket-launch"></i>&ensp;tysm.dev/github`;
+      return;
   }
 
-  document
-    .getElementById("centered")
-    .getElementsByTagName(
-      "p"
-    )[0].innerHTML = `<i class="fa-brands fa-discord"></i>&ensp;${data.state}`;
+  document.getElementById("centered").getElementsByTagName("p")[0].innerHTML = `<i class="fa-brands fa-discord"></i>&ensp;${data.state}`;
 }
 
 async function SpotifyNP() {
   let res = await fetch("https://spotify.tysm.dev/").then((res) => res.json());
 
   if (res.isPlaying === false) {
-    document
-      .getElementById("spotify")
-      .getElementsByTagName(
-        "h2"
-      )[0].innerHTML = `<i class="fa-brands fa-spotify"></i>&ensp;Nothing playing...`;
-    return;
+      document.getElementById("spotify").getElementsByTagName("h2")[0].innerHTML = `<i class="fa-brands fa-spotify"></i>&ensp;Nothing playing...`;
+      return;
   }
 
   if (res.explicit === true) {
-    document
-      .getElementById("spotify")
-      .getElementsByTagName(
-        "h2"
-      )[0].innerHTML = `<i class="fa-brands fa-spotify"></i>&ensp;Nothing playing...`;
-    return;
+      document.getElementById("spotify").getElementsByTagName("h2")[0].innerHTML = `<i class="fa-brands fa-spotify"></i>&ensp;Nothing playing...`;
+      return;
   }
 
-  document
-    .getElementById("spotify")
-    .getElementsByTagName(
-      "h2"
-    )[0].innerHTML = `<i class="fa-brands fa-spotify"></i>&ensp;<a href="${res.data.trackurl.spotify}">${res.data.track}</a> by ${res.data.artist}`;
+  document.getElementById("spotify").getElementsByTagName("h2")[0].innerHTML = `<i class="fa-brands fa-spotify"></i>&ensp;<a href="${res.data.trackurl.spotify}">${res.data.track}</a> by ${res.data.artist}`;
 }
 
 async function consoleInformation() {
   console.log("%c🛑STOP🛑", "font-size: 30px");
-  console.log(
-    "%cThis website was specific developed for tysm.dev only.",
-    "font-weight: bold; font-size: 30px"
-  );
+  console.log("%cThis website was specific developed for tysm.dev only.", "font-weight: bold; font-size: 30px");
 }
 
 async function Intervals() {
   getStatus();
-  // ? SpotifyNP(); ! Removed due to rate limits
+  SpotifyNP();
   consoleInformation();
   setInterval(Intervals, 50000);
 }
