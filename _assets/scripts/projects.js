347ba32faacaa5a7ff86c006f23f4d2321483e58
Crispy <61794525+Crispy-Cream@users.noreply.github.com>
2022-07-07 22:47:51 -0500

    project page soon

---
+++ _assets/scripts/projects.js
@@ -0,0 +1,7 @@
+function onLoad () {
+    console.log("%c🛑STOP🛑", "font-size: 30px");
+    console.log(
+      "%cThis website was specific developed for tysm.dev only.",
+      "font-weight: bold; font-size: 30px"
+    );
+}
